5ee52e6d2300f71a6cac467bf0b4d1bc32927a20
NeuralFlux <40491005+NeuralFlux@users.noreply.github.com>
2024-11-05 12:51:15 -0500

    fix: add file locking mechanism

--- src/misc.ts
+++ src/misc.ts
@@ -1,3 +1,6 @@
+import lockfile from "proper-lockfile";
+import { setTimeout as sleep } from "timers/promises";
+
 export function toArray<Type>(input: Type | Type[]): Type[] {
   if (Array.isArray(input)) {
     return input;
@@ -75,4 +78,63 @@ export function timeoutPromise<T>(promise: Promise<T>, timeout: number): Promise
     resolve = newResolve;
     reject = newReject;
   });
+}
+
+export const LOCKFILE_STALENESS = {stale: 5000}; // lock expiration in milliseconds to prevent deadlocks
+export const LOCKFILE_RETRY_CONFIG = {
+  retries: {
+    retries: 10,
+    factor: 2,
+    minTimeout: 100,
+    maxTimeout: 1000,
+  },
+  stale: LOCKFILE_STALENESS["stale"],
+};
+
+export async function lockWithActionAsync<T>(filePath: string, action: () => Promise<T>, debug?: (message: string) => void): Promise<T> {
+  if (process.env.NODE_ENV !== "production") {
+    debug(`Development mode: Skipping lockfile ${process.env.NODE_ENV}`);
+    const result = await action();
+    return result;
+  }
+
+  let release;
+  try {
+    release = await lockfile.lock(filePath, LOCKFILE_RETRY_CONFIG);
+    const result = await action();
+    return result;
+  } catch (error) {
+    debug(`Lockfile error: ${error}`);
+    // throw error;
+  } finally {
+    if (release) release();
+  }
+}
+
+export function lockWithActionSync<T>(filePath: string, action: () => T, debug?: (message: string) => void): T {
+  if (process.env.NODE_ENV !== "production") {
+    debug(`Development mode: Skipping lockfile ${process.env.NODE_ENV}`);
+    return action();
+  }
+
+  let release;
+  try {
+    const startTime = Date.now();
+
+    while (Date.now() - startTime < LOCKFILE_STALENESS["stale"]) {
+      if (!lockfile.checkSync(filePath)) {
+        release = lockfile.lockSync(filePath, LOCKFILE_STALENESS);
+        const result = action();
+        return result;
+      } else {
+        sleep(LOCKFILE_RETRY_CONFIG["retries"]["minTimeout"]);
+      }
+    }
+    debug("Lockfile timeout: did not read file");
+  } catch (error) {
+    debug(`Lockfile error: ${error}`);
+    // throw error;
+  } finally {
+    if (release) release();
+  }
 }
